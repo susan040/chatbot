61c0081a741d4a9b95794b613a25376d4731a62f
susan040 <136952451+susan040@users.noreply.github.com>
2024-03-11 12:29:45 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:chatbot/chat.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gemini_bot/flutter_gemini_bot.dart';
+import 'package:flutter_gemini_bot/models/chat_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Gemini Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ChatPage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<ChatModel> chatList = []; // Your list of ChatModel objects
+  String apiKey = 'AIzaSyDW11yZsVMSlHzN1q3IgJS9GXjgtiNVh9A';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: FlutterGeminiChat(
+        chatContext: 'you are a frontend app developer',
+        chatList: chatList,
+        apiKey: apiKey,
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
